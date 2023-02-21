ff06b1f5bb4bbb1d8cc05645520d815f67d2237b
[DAGOATTTT] <104139331+DAGOATTTT@users.noreply.github.com>
2023-02-21 17:01:59 -0600

    Goals controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,40 +1,52 @@
+
+const asycnHandler = require('express-async-handler');
+
 // @description Get Goals
 //@route GET /api/goals
 //@access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler (async (req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 
 // @description Set Goals
 //@route POST /api/goals
 //@access Private
-const setGoal = (req, res) => {
-    res.status(200).json({message: 'Get goals'})
-}
-
-
-// @description Get Goals
-//@route GET /api/goals
-//@access Private
-const getGoals = (req, res) => {
-    res.status(200).json({message: 'Get goals'})
-}
-
-
+const setGoal = asyncHandler (async (req, res) => {
+    console.log(req.body)
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(200).json({message: 'Set goals'})
+})
 
 
 
 // @description Get Goals
-//@route GET /api/goals
+//@route PUT /api/goals/id
 //@access Private
-const getGoals = (req, res) => {
-    res.status(200).json({message: 'Get goals'})
-}
+const updateGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+
+
+
+
+
+// @description Get Goals
+//@route DELETE /api/goals
+//@access Private
+const deleteGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id} `})
+})
 
 
 
 
 module.exports = {
     getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
