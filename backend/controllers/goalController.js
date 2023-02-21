05675f61484f1da26213e09bb8b472136bb37814
[DAGOATTTT] <104139331+DAGOATTTT@users.noreply.github.com>
2023-02-21 09:33:20 -0600

    half-way there

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,40 @@
+// @description Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+
+// @description Set Goals
+//@route POST /api/goals
+//@access Private
+const setGoal = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+
+// @description Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+
+
+
+
+// @description Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+
+
+
+module.exports = {
+    getGoals,
+}
